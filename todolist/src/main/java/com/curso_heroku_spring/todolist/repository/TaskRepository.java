097f796dbc0a514c977ad3e9b7ee2b76ce0e1ffa
Thiago <thiago.bafi@outlook.com>
2024-08-14 15:10:56 -0300

    Update TaskRepository.java

--- todolist/src/main/java/com/curso_heroku_spring/todolist/repository/TaskRepository.java
+++ todolist/src/main/java/com/curso_heroku_spring/todolist/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.curso_heroku_spring.todolist.repository;
 
+import com.curso_heroku_spring.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
