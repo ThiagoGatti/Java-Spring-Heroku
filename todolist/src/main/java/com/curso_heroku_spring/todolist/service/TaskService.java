7c011366bb03e4e9ec6fb7bec9798c5f72977769
Thiago <thiago.bafi@outlook.com>
2024-08-14 15:10:54 -0300

    Update TaskService.java

--- todolist/src/main/java/com/curso_heroku_spring/todolist/service/TaskService.java
+++ todolist/src/main/java/com/curso_heroku_spring/todolist/service/TaskService.java
@@ -1,4 +1,20 @@
 package com.curso_heroku_spring.todolist.service;
 
+
+import com.curso_heroku_spring.todolist.model.Task;
+import com.curso_heroku_spring.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
 }
