05c2b2ea232ffffb42523c87947637a09f656e60
Thiago <thiago.bafi@outlook.com>
2024-08-14 14:56:09 -0300

    first setup

---
+++ todolist/src/test/java/com/curso_heroku_spring/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_heroku_spring.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
