4b351b7b03eeb70681e5563ae1b1f93ad6c44c46
Thiago <thiago.bafi@outlook.com>
2024-08-14 15:10:58 -0300

    Update TaskController.java

--- todolist/src/main/java/com/curso_heroku_spring/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/curso_heroku_spring/todolist/controller/TaskController.java
@@ -1,4 +1,20 @@
 package com.curso_heroku_spring.todolist.controller;
 
+import com.curso_heroku_spring.todolist.model.Task;
+import com.curso_heroku_spring.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
 }
